e2d63a3c711fe2d16608c1fe622eb05d7ca07fb5
Miguel Montoya <mamt222007@gmail.com>
2026-03-24 19:00:25 -0500

    feat: crear clase Usuario

---
+++ models/Usuario.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace libreria_montoya.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public bool Activo { get; set; }
+
+        public Usuario()
+        {
+            Activo = true;
+        }
+
+        public Usuario(int id, string nombre, string email)
+        {
+            Id = id;
+            Nombre = nombre;
+            Email = email;
+            Activo = true;
+        }
+
+        public string ResumenCorto()
+        {
+            return $"{Nombre} - {Email}";
+        }
+
+        public string DetalleCompleto()
+        {
+            return $"ID: {Id}\nNombre: {Nombre}\nEmail: {Email}\nActivo: {Activo}";
+        }
+
+        public override string ToString()
+        {
+            return DetalleCompleto();
+        }
+    }
+}
